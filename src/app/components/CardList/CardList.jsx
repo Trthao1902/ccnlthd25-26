d388bf1fbce004520a6dca3ccfd1904eac4578d1
Trthao1902 <148039584+Trthao1902@users.noreply.github.com>
2026-04-06 18:30:20 +0700

    Update API endpoint to production URL

--- src/app/components/CardList/CardList.jsx
+++ src/app/components/CardList/CardList.jsx
@@ -4,7 +4,7 @@ import Pagination from '../Pagination/Pagination'
 import Card from '../Card/Card'
 
 const getData = async (page, cat) => {
-  const res = await fetch(`http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`, {
+  const res = await fetch(`https://ccnlthd25-26-adqe.vercel.app/api/posts?page=${page}&cat=${cat || ""}`, {
     cache: "no-store", 
     });
 
